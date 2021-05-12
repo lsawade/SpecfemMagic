854aca3dd6980b8e4584da556128639edd3c1e02
Lucas Sawade <lsawade@princeton.edu>
2021-05-12 10:28:57 -0400

    Added default parameters and changed to my specfem branch

--- 01_get_repositories.sh
+++ 01_get_repositories.sh
@@ -7,7 +7,7 @@ source 00_compilations_parameters.sh
 if [ ! -d $SPECFEM_DIR ]; then
     git clone $SPECFEM_LINK
     cd specfem3d_globe
-    git checkout -b devel origin/devel
+    git checkout -b devel origin/devel-lucas
     cd $CURRENT_DIR
 fi
 
