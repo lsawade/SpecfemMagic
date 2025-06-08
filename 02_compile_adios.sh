674d3d373d7e8a8a5742747ca5da9018646b44fd
Lucas Sawade <lsawade@princeton.edu>
2025-06-08 15:14:19 -0400

    Intel compilation works after gcc has bug when loading hdf5

--- 02_compile_adios.sh
+++ 02_compile_adios.sh
@@ -12,11 +12,15 @@ then
     cd $ADIOS_DIR
     mkdir $ADIOS_BUILD
     cd $ADIOS_BUILD
-    CC="$(which mpicc)" CXX="$(which mpicxx)" MPICC="$(which mpicc)" \
+    rm -f ${ADIOS_BUILD}/CMakeCache.txt
     cmake -DCMAKE_INSTALL_PREFIX=${ADIOS_INSTALL}  \
+          -DCMAKE_BUILD_TYPE=Release \
           -DADIOS2_USE_MPI=ON \
           -DADIOS2_USE_FORTRAN=ON \
           -DADIOS2_USE_HDF5=OFF \
+          -DCMAKE_C_COMPILER=${MPICC} \
+          -DCMAKE_Fortran_COMPILER=${MPIFC} \
+          -DCMAKE_CXX_COMPILER=${MPICXX} \
           ../adios
     make -j 16
     make install
